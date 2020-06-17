595b4f83cd688017326d3ec87a598cd49bc76ebf
achoi616 <achoi616@gmail.com>
2020-06-17 19:13:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(this, that)
   this = ["raindrops on roses", "whiskers on kittens"]
   that = ["sports cars", "flatiron school"]
-  the = this.concat(that)
+  @my_favorite_things = this.concat(that)
 end
