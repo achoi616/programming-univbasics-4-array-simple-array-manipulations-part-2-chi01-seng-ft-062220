61f4ea580f8c966218f9c6a809cd9af9f2845904
achoi616 <achoi616@gmail.com>
2020-06-17 19:24:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,3 +8,8 @@ def using_insert(this, that)
   this = ["1", "2", "3", "4", "6", "7", "8"]
   this.insert(4, "Python")
 end
+
+def using_uniq(this, that)
+  this = ["1", "2", "3", "4", "5", "6", "4"]
+  this.uniq
+end
