974355559eb15cca969bad230329d4ff4a27e904
achoi616 <achoi616@gmail.com>
2020-06-17 19:29:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,7 +9,7 @@ def using_insert(this, that)
   this.insert(4, "Python")
 end
 
-def using_uniq(this, that)
-  this = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  that = this.uniq
+def using_uniq(haircuts, new)
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  new = haircuts.uniq
 end
