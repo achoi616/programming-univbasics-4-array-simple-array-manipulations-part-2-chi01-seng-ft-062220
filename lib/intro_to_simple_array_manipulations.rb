c41423205b7a3b998979189d8329ea60dd2d964d
achoi616 <achoi616@gmail.com>
2020-06-17 19:45:01 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_concat(this, that)
+  this = ["raindrops on roses", "whiskers on kittens"]
+  that = ["sports cars", "flatiron school"]
+  @my_favorite_things = this.concat(that)
+end
+
+def using_insert(this, that)
+  this = ["1", "2", "3", "4", "6", "7", "8"]
+  this.insert(4, "Python")
+end
+
+def using_uniq(haircuts)
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  new_array = haircuts.uniq
+end
+
+def using_flatten(array)
+  array = ["Saxophone", "Piano", "Trumpet", ["Violin", "Drums", "Flute"]]
+  array.flatten
+end
+
+def using_delete(instructors, fired)
+  #instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  instructors.delete("Steven")
+end
+
+def using_delete_at(array, integer)
+  array = ["Robocop", "2", "3"]
+  array.delete_at(0)
+end
