b0d8ffe96f87c301c62f101573fd697ddcaa8ef4
achoi616 <achoi616@gmail.com>
2020-06-17 19:17:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,11 @@ def using_concat(this, that)
   that = ["sports cars", "flatiron school"]
   @my_favorite_things = this.concat(that)
 end
+
+def using_insert(this, that)
+  this = ["1", "2", "3", "4"]
+  that = ["5"]
+  @new_array = this.insert(that)
+end
+
+  
